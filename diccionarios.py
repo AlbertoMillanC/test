a4d0a98a0a27ad8dc4d843ca5a37fba0c8c42bb9
beicon <carlosalbertomillanc@gmail.com>
2022-05-14 23:09:04 -0500

    ejercicios de diccionarios mostrar valores

--- diccionarios.py
+++ diccionarios.py
@@ -1,9 +1,17 @@
-lenguaje  = {
+"""lenguaje  = {
     "name": "python",
     "creacion": "Guido Van Rossum"
     
     }
-for key in lenguaje:
-    print("key", key)
-    print("valor", lenguaje[key])
-    
+
+#for key in lenguaje:
+    #print("key", key)
+   # print("valor", lenguaje[key])
+   
+#for elemento in lenguaje.values():
+    #print(elemento)    
+for key,value in lenguaje.items():
+    print(key,value)
+
+"""
+frutas = {"fresa": "roja", "manzana": "verde", "plátano" : "amarillo"}
