ab95e674f096b2d28474ba0d145c7444acf43a90
beicon <carlosalbertomillanc@gmail.com>
2022-05-16 15:19:05 -0500

    son ejercicios de diccionario y evaluación

--- diccionarios.py
+++ diccionarios.py
@@ -14,7 +14,7 @@ for key,value in lenguaje.items():
     print(key,value)
 
 """
-frutas = {"fresa": "roja", "manzana": "verde", "plátano" : "amarillo"}
+#frutas = {"fresa": "roja", "manzana": "verde", "plátano" : "amarillo"}
 
 """for fruta in frutas.keys():
     print(fruta)"""
@@ -45,11 +45,18 @@ y = 4
 for y in range(11):
     print(x, " x ", y, " = ", x*y)"""
     
-def perimetro_cuadrado(lado,unidades):
+"""y = 4def perimetro_cuadrado(lado,unidades):
     perimetro = lado * 4
-    print(f"El perimetro del cuadrado es:", (perimetro), (unidades))
-    
-perimetro_cuadrado(25,"metros")
+    print(f"El perimetro del cuadrado es:", (perimetro), (unidades))"""
+"""def mensaje(men):
+    y = ""
+    for x in men:
+       y = y + x
+       print(y)
 
+mensaje("hola")"""
+def area_rectangulo():
+       return lado * lado
 
+print= (area_rectangulo)
    
